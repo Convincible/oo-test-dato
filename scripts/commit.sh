4bd1604ad41f947e9ddc9d4375c947807ce485ae
Convincible <development@convincible.media>
2021-08-22 17:46:31 +0100

    Push commit

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -3,4 +3,5 @@ echo "Attempting to commmit files to Git..."
 git config user.email "development@convincible.media"
 git config user.name "Convincible"
 git add ./source/content && git commit -m "Automated content update from DatoCMS edits"
+git push
 exit 0 # Return success even if Git did not commit, to allow build process to continue
