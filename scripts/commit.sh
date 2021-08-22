a14530ff0f020578b2b084a66ef1e68016f96827
Convincible <development@convincible.media>
2021-08-22 19:23:17 +0100

    Combine scripts

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,6 +1,31 @@
 #!/usr/bin/env bash
 echo "Attempting to commmit files to Git..."
 
+#!/usr/bin/env bash
+echo "Adding SSH from Environment Variables"
+
+# Check if we're running in a Netlify environment
+# See https://www.netlify.com/docs/continuous-deployment/#environment-variables
+#if [ "$NETLIFY" = "true" ]
+#then
+	dir=~/.ssh
+
+    # Init .ssh dir and expand $SSH_KEY
+    mkdir -p "$dir"
+    echo -e "-----BEGIN OPENSSH PRIVATE KEY-----\\n${DEPLOY_KEY//_/\\n}\\n-----END OPENSSH PRIVATE KEY-----" >> "$dir/${SITE_ID}"
+    chmod og-rwx "$dir/${SITE_ID}"
+	eval `ssh-agent -s`
+    ssh-add "$dir/${SITE_ID}"
+
+    # Uncomment to debug
+    #ls -la "$dir"
+    #cat "$dir/${SITE_ID}"
+
+    # Add host keys, comment out if not needed
+    #ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+#fi
+
+
 echo "CURRENT WORKING DIRECTORY"
 pwd
 echo "DIRECTORY LISTING"
@@ -24,4 +49,6 @@ git add ./source/content && git commit -m "Automated content update from DatoCMS
 echo "PUSH"
 git push origin HEAD:master
 echo "DONE"
+
+
 exit 0 # Return success even if Git did not commit, to allow build process to continue
